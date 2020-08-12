b09eb2ba914942ac743c22ca62614ea72ea51586
nakanishi <nigiiro7716@gmail.com>
2020-08-12 18:19:03 +0900

    add header

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,26 @@
 
 // Bootstrap
 //= require bootstrap-sprockets
+
+
+$(window).scroll(function () {
+    //Header
+    if ($(window).scrollTop() >= 100) {
+        $('.header').addClass('fixed-top');
+    }
+    else {
+        $('.header').removeClass('fixed-top');
+    }
+    //Footer
+    var navpanel = $('.bottom-nav');
+    var panelHeight = navpanel.height() - 4;
+    var footerHeight = $('.footer').height();
+    if ($(document).height() - panelHeight - footerHeight <= $(window).scrollTop() + $(window).height()) {
+        navpanel.removeClass('fixed-bottom');
+    }
+    else {
+        navpanel.addClass('fixed-bottom');
+    }
+
+});
+
